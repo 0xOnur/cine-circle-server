c9f99a5f481db2b0484d1f21cf588c52acf35111
0xOnur <onurabat3@gmail.com>
2024-04-12 01:45:31 +0300

    Add initial project files

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import {createServer} from "http";
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+
+dotenv.config();
+
+export const app = express();
+export const server = createServer(app);
+const clientOrigin = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(cors({ credentials: true, origin: clientOrigin }));
+app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 5000;
+mongoose.set("strictQuery", false);
+mongoose.connect(String(process.env.CONNECTION_URL)).then(() => {
+    server.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+        console.log(`Serving client from origin: ${clientOrigin}`);
+    });
+}).catch((error) => {
+    console.log(error.message);
+});
